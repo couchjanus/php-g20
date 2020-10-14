cac3790c78889b613c09e513ca68573f4ebcf51a
user.bane:"couchjanus@gmail.com <couchjanus@gmail.com>
2020-10-14 16:33:21 +0300

    added test

--- core/Request.php
+++ core/Request.php
@@ -5,22 +5,13 @@ class Request
     private $request; // переменная хранящая данные GET и POST
    
     public function __construct() {
-        // $this->request = $_REQUEST;
-        // при создании объекта запроса мы пропускаем все данные
-        // через фильтр-функцию для очистки параметров от нежелательных данных
-        // clear data from dangerous characters
-        $this->request = $this->cleanInput($_REQUEST);
-    
+        $this->request = $this->cleanInput($_REQUEST);    
     }
 
-    // магическая функция, которая позволяет обращатья к GET и POST переменным
-    // по имени, например, echo $request->id
     public function __get($name) {
         if (isset($this->request[$name])) return $this->request[$name];
     }
    
-    // очистка данных от опасных символов
-    // clearing data from dangerous characters
     private function cleanInput($data) {
         if (is_array($data)) {
             $cleaned = [];
@@ -32,9 +23,17 @@ class Request
         return trim(htmlspecialchars($data, ENT_QUOTES));
     }
    
-    // return the request content
     public function getRequestEntries()
     {
         return $this->request;
+    }
+
+    public static function uri()
+	{
+        return isset($_SERVER['REQUEST_URI'])? trim($_SERVER["REQUEST_URI"], '/'): null;
+    }
+    public static function method()
+    {
+        return $_SERVER['REQUEST_METHOD'];
     }
 }
